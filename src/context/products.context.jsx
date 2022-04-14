9146768c6c00b1cd233e8eacd96cdfa15bd77dfd
Natalia Sowa <natalia.sowaa@gmail.com>
2022-04-14 16:01:52 +0200

    mapping categories

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,15 +1,21 @@
 import { createContext, useState, useEffect } from "react";
 
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
-
-
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
   products: [],
 });
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  });
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
