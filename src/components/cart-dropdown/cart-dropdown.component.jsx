27dfb084c0ab8bef651555a5b0eef611ebec24c4
Natalia Sowa <natalia.sowaa@gmail.com>
2022-04-15 18:08:35 +0200

    adding message ti cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,16 +12,18 @@ const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
-const goToCheckoutHandler =()=>{
-  navigate('/checkout')
-}
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+        cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
